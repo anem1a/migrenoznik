8ca071093014fbee15fb0cff890fe3648aace470
Dmitry Balabanov <dmittrj@outlook.com>
2025-10-10 20:44:59 +0300

    Add light template of the main page

--- web/index.php
+++ web/index.php
@@ -1,11 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="RU">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Дневник мигрени</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/style/main.css?17092501">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
+	<meta name="keywords" content="дневник мигрени, мигренозник, дневник">
+	<meta name="description" content="Мигренозник &mdash; дневник мигрени">
+    <title>Мигренозник &mdash; дневник мигрени</title>
 </head>
 <body>
-    
+    <div class="migre-v1-content">
+        <div class="migre-v1-container">
+            <div class="migre-v1-main">
+                <div class="migre-v1-main-diary">
+                    <div class="migre-v1-main-diary-wrapper">
+                        <div class="migre-v1-main-diary-item"></div>
+                    </div>
+                </div>  
+                <div class="migre-v1-main-bottom">
+                    <div class="migre-v1-main-bottom-row">
+                        <div class="migre-v1-main-bottom-button-wrapper">
+                            <button class="migre-v1-main-bottom-button">Отметить мигрень сейчас</button>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+        </div>
+    </div>
 </body>
 </html>
