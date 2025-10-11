4ff97449b7f891865fec27aa699f689b03270c81
Dmitry Balabanov <dmittrj@outlook.com>
2025-10-11 13:28:40 +0300

    Add storing migraine attacks in local storage

--- web/assets/scripts/diary/main.js
+++ web/assets/scripts/diary/main.js
@@ -1,4 +1,11 @@
 
+class MigraineAttack {
+    constructor(dt_start, dt_end = null) {
+        this.DT_Start = dt_start;
+        this.DT_End = dt_end;
+    }
+}
+
 class MigrenoznikCore {
 
     /**
@@ -25,6 +32,47 @@ class MigrenoznikCore {
         }
     }
 
+    /**
+     * Returns the entire diary of migraine attacks.
+     */
+    get_migraine_attacks() {
+
+    }
+
+    /**
+     * Adds new migraine attack to the end of the list.
+     */
+    add_new_migraine_attack(migraine_attack) {
+        let migraine_attacks = localStorage.getItem("migraine_attacks");
+        if (migraine_attacks == undefined) {
+            localStorage.setItem("migraine_attacks", JSON.stringify([migraine_attack]));
+            return;
+        }
+        try {
+            migraine_attacks = JSON.parse(migraine_attacks);
+            migraine_attacks.push(migraine_attack);
+            localStorage.setItem("migraine_attacks", JSON.stringify(migraine_attacks));
+        } catch (error) {
+            localStorage.setItem("migraine_attacks", JSON.stringify([migraine_attack]));
+        }
+    }
+
+    close_last_migraine_attack() {
+        let migraine_attacks = localStorage.getItem("migraine_attacks");
+        if (migraine_attacks == undefined) {
+            return;
+        }
+        try {
+            migraine_attacks = JSON.parse(migraine_attacks);
+            let last_element = migraine_attacks.pop();
+            last_element["DT_End"] = new Date();
+            migraine_attacks.push(last_element);
+            localStorage.setItem("migraine_attacks", JSON.stringify(migraine_attacks));
+        } catch (error) {
+            return;
+        }
+    }
+
     constructor() {
         
     }
@@ -36,12 +84,13 @@ class MigrenoznikCore {
 function migraine_now_button_Clicked() {
     if (Core.is_migraine_now()) {
         Core.toggle_migraine_status();
+        Core.close_last_migraine_attack();
         document.getElementById("migre-diary-main-bottom-button").innerText = "Отметить мигрень сейчас";
     } else {
         Core.toggle_migraine_status();
+        Core.add_new_migraine_attack(new MigraineAttack(new Date()));
         document.getElementById("migre-diary-main-bottom-button").innerText = "Отметить конец мигрени";
     }
 }
-
 
 const Core = new MigrenoznikCore();
