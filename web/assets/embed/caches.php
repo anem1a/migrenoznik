fbbc5c87266541634fd3a77308b6f80203c78e5c
Dmitry Balabanov <dmittrj@outlook.com>
2025-10-11 09:48:19 +0300

    Add function summary

--- web/assets/embed/caches.php
+++ web/assets/embed/caches.php
@@ -8,7 +8,14 @@
         "assets/scripts/diary/pageload.js" => "11.10.25.1",
     );
 
-    function include_file($filename) {
+    /**
+     * This functions prepares path to include file with inserting cache salt.
+     * 
+     * Cache salt is a number represents a date of last change of the file (or random number if file is unlisted).
+     * @param string $filename Path to style or script to include (starts with "assets/...")
+     * @return string Path with cache salt at the end (e.g. "assets/style.css?2756328")
+     */
+    function include_file(string $filename) : string {
         global $CACHES;
         if (array_key_exists($filename, $CACHES)) {
             return $filename . "?" . str_replace(".", "", $CACHES[$filename]);
