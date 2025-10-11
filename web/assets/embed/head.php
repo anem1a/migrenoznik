a406f0eeeca2f23e944624b32634768df3c7dd52
Dmitry Balabanov <dmittrj@outlook.com>
2025-10-11 10:33:53 +0300

    Add two favicons

--- web/assets/embed/head.php
+++ web/assets/embed/head.php
@@ -1,5 +1,8 @@
 <?php 
     require_once __DIR__ . "/caches.php";
+
+    $FAVICONS_PNG = ["120x120"];
+    $FAVICONS_TOUCH = ["180x180"];
 ?>
 <head>
     <meta charset="UTF-8">
@@ -9,6 +12,20 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/<?php echo include_file("assets/style/main.css"); ?>">
+
+    <?php
+        foreach ($FAVICONS_PNG as $item) {
+    ?>
+    <link type="image/png" sizes="<?php echo $item; ?>" rel="icon" href="<?php echo $path_to_root; ?>assets/images/favicons/favicon-<?php echo $item; ?>.png">
+    <?php
+        }
+
+        foreach ($FAVICONS_TOUCH as $item) {
+    ?>
+    <link sizes="<?php echo $item; ?>" rel="apple-touch-icon" href="<?php echo $path_to_root; ?>assets/images/favicons/apple-<?php echo $item; ?>.png">
+    <?php
+        }
+    ?>
 
     <script src="/<?php echo include_file("assets/scripts/lib/browser.js"); ?>"></script>
     <script src="/<?php echo include_file("assets/scripts/diary/pageload.js"); ?>"></script>
