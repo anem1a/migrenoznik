e79e05bc9a402fdfbd57b636bac51bc8c7fbae60
Dmitry Balabanov <dmittrj@outlook.com>
2025-11-27 19:05:15 +0300

    Generate table

--- web/static/assets/scripts/diary/doctor.js
+++ web/static/assets/scripts/diary/doctor.js
@@ -7,7 +7,20 @@ function patient() {
         return response.json();
     })
     .then(data => {
-        alert(JSON.stringify(data, null, 2));
+        document.getElementById("migre-table").innerHTML = `<tr>
+            <th>Дата</th>
+            <th>Длительность</th>
+            <th>Интенсивность</th>
+            <th>Триггеры</th>
+        </tr>`;
+        for (const entry of data["entries"]) {
+            document.getElementById("migre-table").innerHTML += `<tr>
+                <td>${entry["DT_Start"]}</td>
+                <td>${entry["Duration"]} ч.</td>
+                <td>${entry["Strength"]}/10</td>
+                <td>${entry["Triggers"].join(", ")}</td>
+            </tr>`;
+        }
     })
     .catch(error => {
         alert('Произошла ошибка: ' + error.message);
