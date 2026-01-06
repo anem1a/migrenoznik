5352da833a7f258c90501f4d5869f4faa19978c2
Dmitry Balabanov <dmittrj@outlook.com>
2026-01-06 14:02:21 +0300

    Delete obsolete code

--- web/static/assets/scripts/diary/classes/MigraineTrigger.js
+++ web/static/assets/scripts/diary/classes/MigraineTrigger.js
@@ -2,12 +2,6 @@ class MigraineTrigger {
     static total_triggers() {
         return Object.keys(Core.Triggers).length;
     }
-    /**
-     * Depricated.
-     */
-    static code_to_name(code) {
-        return Core.Triggers[String(code)];
-    }
 
     constructor(code) {
         this.Code = code;
